c687aafc761af4cafe2367a5a438a9f940f1611f
teo-mateo <teomati@gmail.com>
2013-10-25 14:53:33 +0200

    Update IsidoreSettings.java
    -

--- src/com/tbardici/isidore/IsidoreSettings.java
+++ src/com/tbardici/isidore/IsidoreSettings.java
@@ -1,6 +1,6 @@
 package com.tbardici.isidore;
 
 public class IsidoreSettings {
-	public static String CLIENT_ID = "fySa8zg92lvd29VmTDYJ6w";
-	public static String API_KEY = "cc085f765fd8804e425cfc65f13cc028";
+	public static String CLIENT_ID = "";
+	public static String API_KEY = "";
 }
